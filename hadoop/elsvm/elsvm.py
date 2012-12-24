dd9efd16651c1ec311d2dfff8c116bc22cdd1295
tianweidut <liutianweidlut@qq.com>
2012-12-24 17:38:26 +0800

    update elsvm

--- hadoop/elsvm/elsvm.py
+++ hadoop/elsvm/elsvm.py
@@ -1,99 +1,95 @@
 # -*- coding: UTF-8 -*-
 '''
-Created on 2012-12-19
+Created on 2012-12-24
 
 @author: tianwei
 '''
 import sys
-from operator import itemgetter
+from math import e
 
 import numpy as np
 import dumbo
 
+from debug import debug
+
 __author__ = "tianwei"
-__date__ = "December 19  2012"
-__description__ = "pca algorithm for the whole date set "
+__date__ = "December 24 2012"
+__description__ = "el-svm for the whole splited block data"
 
 SEP = ','   # Parse from file
-
-
-def debug(content, pos=None):
-    print >> sys.stderr, "+" * 15
-    if pos is not None:
-        print >> sys.stderr, pos
-    print >> sys.stderr, content
-    print >> sys.stderr, "-" * 15
 
 
 class Mapper():
     def __init__(self):
         """
         """
-        self.means = self.load_means()
+        self.w_matrix = self.load_w_matrix()
 
-    def load_means(self):
+    def load_w_matrix(self):
         """
         load means from the file
         """
-        f = open(self.params["means_filename"])
+        f = open(self.params["w_matrix_filename"])
         content = f.readlines()
-        matrix = [0.0 for i in content]
+
+        matrix = None
 
         for line in content:
-            word = line.split("\t")
-            matrix[int(word[0])] = float(word[1])
+            point = np.fromstring(line, dtype=np.float64, sep=SEP)
+            point = np.array([point])
+            matrix = np.concatenate((matrix, point)) if matrix is not None else point
 
         f.close()
 
-        return np.array(matrix)
+        return matrix
 
-    def standardization(self, matrix):
+    def calculate(self, point):
         """
-        standardization the matrix
+        Calculate Program
+
+        Inputs:
+            point, a single-original line data
+
+        Outputs:
+            localH: a(T) * a
+            localD: a * y
         """
-        (rows, cols) = np.shape(matrix)
-        new_matrix = np.zeros((rows, cols))
-        std_matrix = np.std(matrix, 0)
+        y = np.array([point[-1]])
+        point[-1] = 1
+        point = np.dot(point, self.w_matrix)
+        point_list = map(lambda x: 1 / (1 + pow(e, -x)), point.tolist())
+        point = np.concatenate((np.array(point_list), np.array([1])))
 
-        for value in std_matrix:
-            if value == 0:
-                raise ZeroDivisionError('division by zero, cannot proceed')
-
-        for row in range(rows):
-            new_matrix[row, 0:cols] = matrix[row, 0:cols] / \
-                    std_matrix[row, 0:cols]
-
-        return new_matrix
+        return (np.dot(np.transpose(point), point), np.dot(y, point))
 
     def __call__(self, data):
         """
-        Mapper Progra 
-        
-        Inputs 
+        Mapper Program:    
+
+        Inputs: 
             data, which is the whole split block data
 
         Outputs:
             key: untified id
-            value: covariance
+            value: resultD,resultH 
         """
         
-        # SETP1: calculate the means matrix 
-        result = None
+        # SETP1: read data matrix and do some transpose 
+        resultH = None
+        resultD = None
 
         for docID, doc in data:
             for term in doc.split("\n"):
                 point = np.fromstring(term, dtype=np.float64, sep=SEP)
-                point = np.array([point - self.means])
-                result = np.concatenate((result, point)) if result is not None else point
-
-        # SETP2: normalize the matrix
-        #result = self.standardization(result)
-
-        #SETP3: for every column, calculate X(T) * X
-        local_matrix = np.dot(np.transpose(result), result)
-
-        yield 1, local_matrix.tolist()
-
+                (localH, localD) = self.calculate(point) 
+                if resultH is not None:
+                    resultH = resultH + localH
+                    resultD = resultD + localD
+                else:
+                    resultH = localH
+                    resultD = localD
+                
+        yield resultD, resultH
 
 class Reducer(): 
     def __call__(self, key, values):
@@ -107,28 +103,7 @@ class Reducer():
         Outputs:
             the sorted eigenvalue and eigenvector list 
         """
-        s = None
         
-        # SETP4: Combine the array result
-        for v in values:
-            v = np.array(v)
-            if s is None: s = np.zeros(np.shape(v), float)  
-            s = s + v
-
-        # SETP5:singular value decomposition
-        [U, S, V] = np.linalg.svd(s)
-        debug(U) 
-        debug(S)
-        debug(V)
-        # STEP6:sort the SVD and output the matrix(descending order)
-        eigendict = {}
-        for i in range(0, len(S)):
-            eigendict[S[i]] = U[:, i]
-
-        sorted_eigendict = sorted(eigendict.iteritems(), key=itemgetter(0), reverse=True)
-
-        for key, item in sorted_eigendict:
-            yield key, item.tolist()
 
 if __name__ == "__main__":
     dumbo.run(Mapper, Reducer)
