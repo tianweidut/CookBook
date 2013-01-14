40939ed8bda8ce95bc597260bc076438ff5e515b
tianweidut <liutianweidlut@qq.com>
2013-01-14 11:08:51 +0800

    update test files

---
+++ hadoop/elsvm/config_acc.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 2013-01-13
+
+@author: tianwei
+'''
+
+__author__ = "tianwei"
+__date__ = "January 13 2012"
+__description__ = "the data sets of the el-svm, i\
+        Heads up! we should follow the orders "
+
+DATASETS = ("step1.csv",
+            "step2.csv",
+            "step3.csv",
+            "step4.csv",
+            "step5.csv",)
