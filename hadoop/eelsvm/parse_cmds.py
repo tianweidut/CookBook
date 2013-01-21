f0481e5a80f676c09371a054a8f4c7d2b525c702
tianweidut <liutianweidlut@qq.com>
2013-01-21 15:45:12 +0800

    add h_arguments

--- hadoop/eelsvm/parse_cmds.py
+++ hadoop/eelsvm/parse_cmds.py
@@ -16,8 +16,8 @@ __description__ = "command parser for main-wrapper"
 class Parse():
     def run(self):
         """
-        Opt Parse for command line 
-    
+        Opt Parse for command line
+
         Inputs:
             None
 
@@ -26,31 +26,46 @@ class Parse():
             args:
         """
         parse = OptionParser()
-        parse.add_option("-o", "--openHadoop", action="store_true", dest="is_hadoop", 
-                        help="whether it is running on hadoop platform")
-        parse.add_option("-c", "--closeHadoop", action="store_false", dest="is_hadoop", 
-                        help="whether it is running on hadoop platform")
-        parse.add_option("-n", "--num", action="store", dest="num", 
-                        help="num for elsvm")
-        parse.add_option("-v", "--varibles", action="store", dest="var",
-                        help="v for elsvm")
-        parse.add_option("-s", "--sampleName", action="store", dest="sample_name",
-                        default="elsvm_data.csv", help="sample for elsvm")
-        parse.add_option("-p", "--path", action="store", dest="path",
-                        help="sample data path")
-        parse.add_option("-t", "--output_path", action="store", dest="output_path",
-                        help="result output path")
-        parse.add_option("-l", "--local_path", action="store", dest="local_path",
-                        help="local output path")
-        parse.add_option("-m", "--output_name", action="store",
-                        dest="output_name",
-                        help="output name")
-        parse.add_option("-d", "--dim", action="store", dest="dim",
-                        help="dim number for sample data")
-        parse.add_option("-i", "--enableIncrement", action="store_true", dest="is_increment",
-                        help="use increment mode")
-        parse.add_option("-z", "--disableIncrement", action="store_false", dest="is_increment",
-                        help="disable increment mode")
+        parse.add_option("-o", "--openHadoop",
+                         action="store_true", dest="is_hadoop",
+                         help="whether it is running on hadoop platform")
+        parse.add_option("-c", "--closeHadoop",
+                         action="store_false", dest="is_hadoop",
+                         help="whether it is running on hadoop platform")
+        parse.add_option("-n", "--num",
+                         action="store", dest="num",
+                         help="num for elsvm")
+        parse.add_option("-v", "--varibles",
+                         action="store", dest="var",
+                         help="v for elsvm")
+        parse.add_option("-s", "--sampleName",
+                         action="store", dest="sample_name",
+                         default="elsvm_data.csv", help="sample for elsvm")
+        parse.add_option("-p", "--path",
+                         action="store", dest="path",
+                         help="sample data path")
+        parse.add_option("-t", "--output_path",
+                         action="store", dest="output_path",
+                         help="result output path")
+        parse.add_option("-l", "--local_path",
+                         action="store", dest="local_path",
+                         help="local output path")
+        parse.add_option("-m", "--output_name",
+                         action="store",
+                         dest="output_name",
+                         help="output name")
+        parse.add_option("-d", "--dim",
+                         action="store", dest="dim",
+                         help="dim number for sample data")
+        parse.add_option("-i", "--enableIncrement",
+                         action="store_true", dest="is_increment",
+                         help="use increment mode")
+        parse.add_option("-z", "--disableIncrement",
+                         action="store_false", dest="is_increment",
+                         help="disable increment mode")
+        parse.add_option("-h", "--hargument",
+                         action="store", dest="h_argument",
+                         help="new argument for eelsvm")
 
         (options, args) = parse.parse_args()
 
